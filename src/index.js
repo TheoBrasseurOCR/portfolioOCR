441cfcf881cfed8fa4582325d6ad364a9c0b171f
Théo Brasseur <theobrasseurlille@hotmail.com>
2024-02-04 18:07:38 +0100

    min scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/App.scss';
+import './styles/App.min.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    email: '',
-    subject: '',
-    message: '',
+    firstName: "",
+    email: "",
+    subject: "",
+    message: "",
   });
+  const [acceptTerms, setAcceptTerms] = useState(false);
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const handleChange = (e) => {
@@ -17,37 +18,54 @@ const Contact = () => {
     });
   };
 
+  const handleCheckboxChange = () => {
+    setAcceptTerms(!acceptTerms);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!acceptTerms) {
+      console.error(
+        "Veuillez accepter les conditions pour soumettre le formulaire."
+      );
+      return;
+    }
+
     try {
-      const response = await fetch('http://localhost:3001/submit-form', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3001/submit-form", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
 
       if (response.ok) {
-        setSubmitStatus('success');
-        console.log('Formulaire soumis avec succès !');
+        setSubmitStatus("success");
+        console.log("Formulaire soumis avec succès !");
       } else {
-        setSubmitStatus('error');
-        console.error('Erreur lors de la soumission du formulaire. Statut de la réponse:', response.status);
+        setSubmitStatus("error");
+        console.error(
+          "Erreur lors de la soumission du formulaire. Statut de la réponse:",
+          response.status
+        );
       }
     } catch (error) {
-      setSubmitStatus('error');
-      console.error('Erreur lors de la soumission du formulaire :', error);
+      setSubmitStatus("error");
+      console.error("Erreur lors de la soumission du formulaire :", error);
     }
   };
 
   return (
     <section className="contact">
       <div className="contact-container">
-        {/* Condition pour afficher le formulaire ou le message en fonction du statut */}
-        {submitStatus !== 'success' && submitStatus !== 'error' && (
-          <form action="http://localhost:3001/submit-form" method="POST" onSubmit={handleSubmit}>
+        {submitStatus !== "success" && submitStatus !== "error" && (
+          <form
+            action="http://localhost:3001/submit-form"
+            method="POST"
+            onSubmit={handleSubmit}
+          >
             <label htmlFor="firstName">Prénom :</label>
             <input
               type="text"
@@ -87,16 +105,29 @@ const Contact = () => {
               required
             ></textarea>
 
+            <label className="checkbox-label">
+              <input
+                className="check"
+                type="checkbox"
+                checked={acceptTerms}
+                onChange={handleCheckboxChange}
+                required
+              />
+              <p>J'accepte que mes données soient utilisées par le site web.</p>
+            </label>
+
             <button type="submit">Envoyer</button>
           </form>
         )}
 
         {/* Affichage du message en fonction du statut */}
-        {submitStatus === 'success' && (
-          <p style={{ color: '#34495e' }}>Formulaire soumis avec succès !</p>
+        {submitStatus === "success" && (
+          <p style={{ color: "#34495e" }}>Formulaire soumis avec succès !</p>
         )}
-        {submitStatus === 'error' && (
-          <p style={{ color: 'red' }}>Erreur lors de la soumission du formulaire.</p>
+        {submitStatus === "error" && (
+          <p style={{ color: "red" }}>
+            Erreur lors de la soumission du formulaire.
+          </p>
         )}
       </div>
     </section>

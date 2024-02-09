867be7880d3a9facae64402e952b3c17ac50efd1
Théo Brasseur <theobrasseurlille@hotmail.com>
2024-02-09 14:17:12 +0100

    contact edit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,19 @@ const Contact = () => {
       <h1>Contactez moi</h1>
       <h2>Par mail</h2>
       <p>
-        Théo Brasseur - theobrasseurlille@hotmail.com - 07-68-06-45-12 - 27 rue aux Ours, ARRAS 62000, FRANCE.
+        - theobrasseurlille@hotmail.com
+      </p>
+      <h2>Par Téléphone</h2>
+      <p>
+      - 07-68-06-45-12
+      </p>
+      <h2>Par voie postale</h2>
+      <p>
+      - 27 rue aux Ours, ARRAS 62000, FRANCE.
+      </p>
+      <h2>Et sur tous les réseaux sociaux</h2>
+      <p>
+      - @TheoBrasseur
       </p>
     </div>
   );

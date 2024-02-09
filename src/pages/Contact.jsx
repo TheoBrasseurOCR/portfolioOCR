588fbb2f1a870c1582208c377c8b1736b0f5be8c
Théo Brasseur <theobrasseurlille@hotmail.com>
2024-02-09 11:36:49 +0100

    last

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch('https://portfolio-backend-gamma-three.vercel.app', {  
+      const response = await fetch('https://portfolio-backend-gamma-three.vercel.app/submit-form', {  
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
